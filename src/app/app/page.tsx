2030c85294196ba73a03ef743341a0fa47885326
Nyan <nhtun.23@ichat.sp.edu.sg>
2025-11-18 11:11:25 +0800

    Add /app route with Mainpage component integration

---
+++ src/app/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import CommunicationInterface from "../../../whatthehack/src/components/Mainpage";
+
+export default function AppPage() {
+  return <CommunicationInterface />;
+}
+
+
